4eeefa4cd36a91863e0563b09f238321dff0428b
esteban <esteban.abella@minimalart.co>
2024-09-10 15:06:30 -0300

    add styles in notAuthorized component

--- pages/notAuthorized.tsx
+++ pages/notAuthorized.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Button, { ButtonType } from "../components/Button";
+import router from "next/router";
 
 const notAuthorized = () => {
+    const handleClick = () => {
+        router.push('/clients')
+    }
+
     return (
-        <div>
+        <div className="notAuthorized">
             <h1>Not Authorized</h1>
+            <Button type={ButtonType.PRIMARY} value={'Redirect'} onClick={handleClick}></Button>
         </div>
     )
 }
