0bf1628af391a9d4650d8639e34571cb8b97ce6c
esteban <esteban.abella@hotmail.com>
2024-10-05 11:26:27 -0300

    update form styles

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { RootState } from '../redux/rootReducer'
+import React, { useEffect, useState } from "react"
+import { RootState } from "../redux/rootReducer"
 import {
   createUser,
   deleteUser,
   editUser,
   getUser,
   getUsers,
-} from '../redux/user/actions'
-import { UsersReducerPropsTypes } from '../Utils/Types/userType'
-import { connect } from 'react-redux'
-import Button, { ButtonType } from '../components/Button'
-import { ServerStatus } from '../Utils/Types/global'
-import Loader from '../components/Loader'
-import AccessConsume from '../wrappers/auth/AccessConsume'
-import Layout from '../components/Layout'
-import router from 'next/router'
-import Modal from '../components/Modal'
-import { TextFieldType } from '../components/TextField'
-import withAuth from '../hooks/withAuth'
-import Pagination from '../components/Pagination'
-import Search from '../components/Search'
-import { UseCallOfTables } from '../hooks/useCallOfTables'
-import { loggedUser } from '../Utils/Types/authModel'
+} from "../redux/user/actions"
+import { UsersReducerPropsTypes } from "../Utils/Types/userType"
+import { connect } from "react-redux"
+import Button, { ButtonType } from "../components/Button"
+import { ServerStatus } from "../Utils/Types/global"
+import Loader from "../components/Loader"
+import AccessConsume from "../wrappers/auth/AccessConsume"
+import Layout from "../components/Layout"
+import router from "next/router"
+import Modal from "../components/Modal"
+import { TextFieldType } from "../components/TextField"
+import withAuth from "../hooks/withAuth"
+import Pagination from "../components/Pagination"
+import Search from "../components/Search"
+import { UseCallOfTables } from "../hooks/useCallOfTables"
+import { loggedUser } from "../Utils/Types/authModel"
 
 const mapStateToProps = (state: RootState) => {
   const usersReducer = state.user
@@ -71,27 +71,27 @@ const Users = ({
   userCreateStatus,
   userDeleteStatus,
   userEditStatus,
-  userLogged
+  userLogged,
 }: UsersPropType) => {
   const [stateModal, setStateModal] = useState<boolean>(false)
-  const [typeModal, setTypeModal] = useState<string>('')
+  const [typeModal, setTypeModal] = useState<string>("")
   const [dataInitialModal, setDataInitialModal] = useState()
-  const [filter, setFilter] = useState<string>('')
-  const [query, setQuery] = useState<string>('')
+  const [filter, setFilter] = useState<string>("")
+  const [query, setQuery] = useState<string>("")
   const [offsetState, setOffsetState] = useState<number>(0)
   const limit = 5
   const totalItems = users?.count ? users?.count : 0
-  const order = 'ASC'
+  const order = "ASC"
   const roles = userLogged?.role
 
   useEffect(() => {
     getUsers(
       offsetState,
       limit,
-      query ?? '',
-      filter ?? '',
-      order ?? '',
-      roles ?? ''
+      query ?? "",
+      filter ?? "",
+      order ?? "",
+      roles ?? ""
     )
   }, [])
 
@@ -100,16 +100,20 @@ const Users = ({
       const anticipatedTotalItems = totalItems - 1
       const isLastPage = offsetState + limit >= anticipatedTotalItems
 
-      if (isLastPage && anticipatedTotalItems <= offsetState && offsetState > 0) {
+      if (
+        isLastPage &&
+        anticipatedTotalItems <= offsetState &&
+        offsetState > 0
+      ) {
         setOffsetState((prevOffset) => prevOffset - limit)
       } else {
         getUsers(
           offsetState,
           limit,
-          query ?? '',
-          filter ?? '',
-          order ?? '',
-          roles ?? ''
+          query ?? "",
+          filter ?? "",
+          order ?? "",
+          roles ?? ""
         )
       }
       setStateModal(false)
@@ -121,10 +125,10 @@ const Users = ({
       getUsers(
         offsetState,
         limit,
-        query ?? '',
-        filter ?? '',
-        order ?? '',
-        roles ?? ''
+        query ?? "",
+        filter ?? "",
+        order ?? "",
+        roles ?? ""
       )
       setStateModal(false)
     }
@@ -140,10 +144,10 @@ const Users = ({
         getUsers(
           offsetState,
           limit,
-          query ?? '',
-          filter ?? '',
-          order ?? '',
-          roles ?? ''
+          query ?? "",
+          filter ?? "",
+          order ?? "",
+          roles ?? ""
         )
       }
       setStateModal(false)
@@ -163,73 +167,73 @@ const Users = ({
 
   const createUserObject = [
     {
-      label: 'Nombre',
-      name: 'name',
+      label: "Nombre",
+      name: "name",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'text',
-      placeholder: 'Escriba su nombre',
+      type: "text",
+      placeholder: "Escriba su nombre",
     },
     {
-      label: 'E-mail',
-      name: 'email',
+      label: "E-mail",
+      name: "email",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'email',
-      placeholder: 'email@email.com',
+      type: "email",
+      placeholder: "email@email.com",
     },
     {
-      label: 'Password',
-      name: 'password',
+      label: "Password",
+      name: "password",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'password',
-      placeholder: 'Password',
+      type: "password",
+      placeholder: "Password",
     },
     {
-      label: 'Role',
-      name: 'role',
+      label: "Role",
+      name: "role",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'select',
-      placeholder: '-',
-      valueSelect: ['user', 'admin'],
+      type: "select",
+      placeholder: "-",
+      valueSelect: ["user", "admin"],
     },
   ]
 
   const editUserObject = [
     {
-      label: 'Nombre',
-      name: 'name',
+      label: "Nombre",
+      name: "name",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'text',
-      placeholder: 'Escriba su nombre',
+      type: "text",
+      placeholder: "Escriba su nombre",
     },
     {
-      label: 'E-mail',
-      name: 'email',
+      label: "E-mail",
+      name: "email",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'email',
-      placeholder: 'email@email.com',
+      type: "email",
+      placeholder: "email@email.com",
     },
     {
-      label: 'Password',
-      name: 'password',
+      label: "Password",
+      name: "password",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'password',
-      placeholder: 'Password',
+      type: "password",
+      placeholder: "Password",
     },
     {
-      label: 'Role',
-      name: 'role',
+      label: "Role",
+      name: "role",
       typeTextField: TextFieldType.PRIMARY,
       disabled: false,
-      type: 'select',
-      placeholder: '-',
-      valueSelect: ['user', 'admin'],
+      type: "select",
+      placeholder: "-",
+      valueSelect: ["user", "admin"],
     },
   ]
 
@@ -242,11 +246,11 @@ const Users = ({
   }
 
   const handleClickOnModal = (typeModal: string, data?: any) => {
-    if (typeModal === 'create') {
-      setTypeModal('modal-create-user')
+    if (typeModal === "create") {
+      setTypeModal("modal-create-user")
       setStateModal(true)
-    } else if (typeModal === 'edit') {
-      setTypeModal('modal-edit-user')
+    } else if (typeModal === "edit") {
+      setTypeModal("modal-edit-user")
       setStateModal(true)
       setDataInitialModal(data)
     }
@@ -259,46 +263,46 @@ const Users = ({
           stateModal={stateModal}
           setStateModal={setStateModal}
           dataForm={createUserObject}
-          textButton={'Add user'}
+          textButton={"Add user"}
           typeButton={ButtonType.SUCCESS}
           onClick={handleClickCreateUSer}
-          isDisabled={typeModal === 'modal-create-user'}
+          isDisabled={typeModal === "modal-create-user"}
         />
 
         <Modal
           stateModal={stateModal}
           setStateModal={setStateModal}
           dataForm={editUserObject}
-          textButton={'Update user'}
+          textButton={"Update user"}
           typeButton={ButtonType.INFORMATION}
           onClick={handleClickEditUSer}
-          isDisabled={typeModal === 'modal-edit-user'}
+          isDisabled={typeModal === "modal-edit-user"}
           initialData={dataInitialModal}
         />
-        <section className="usersPage">
+        <section className='usersPage'>
           <h1>Users</h1>
           <Search filter={filter} setFilter={setFilter}></Search>
           {usersStatus !== ServerStatus.FETCHING && (
             <>
-              <section className="addUserAction">
+              <section className='addUserAction'>
                 <Button
                   type={ButtonType.SUCCESS}
-                  value={'Add user'}
-                  onClick={() => handleClickOnModal('create')}
-                  extraClassName={'buttonTable'}
+                  value={"Add user"}
+                  onClick={() => handleClickOnModal("create")}
+                  extraClassName={"buttonTable"}
                 ></Button>
               </section>
-              <table className="table table-striped table-bordered">
-                <thead className="table-dark">
+              <table className='table table-striped custom-bg'>
+                <thead className='table-dark tableThead'>
                   <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">E-mail</th>
-                    <th scope="col">Role</th>
-                    <th scope="col">Actions</th>
+                    <th scope='col'>Id</th>
+                    <th scope='col'>Name</th>
+                    <th scope='col'>E-mail</th>
+                    <th scope='col'>Role</th>
+                    <th scope='col'>Actions</th>
                   </tr>
                 </thead>
-                <tbody>
+                <tbody className='tableBody'>
                   {users?.items ? (
                     users?.items.map((data) => (
                       <tr key={data.id}>
@@ -309,26 +313,28 @@ const Users = ({
                               onClick={() =>
                                 router.push(`/userSelected/${data.id}`)
                               }
-                              style={{ cursor: 'pointer' }}
+                              style={{ cursor: "pointer" }}
                             >
                               {data.name}
                             </p>
                           </td>
                           <td>{data.email}</td>
                           <td>{data.role}</td>
-                          <td className="containerButtonTable">
-                            <Button
-                              type={ButtonType.ERROR}
-                              value={'Delete'}
-                              onClick={() => deleteUser(data.id)}
-                              extraClassName={'buttonTable'}
-                            ></Button>
-                            <Button
-                              type={ButtonType.INFORMATION}
-                              value={'Update'}
-                              onClick={() => handleClickOnModal('edit', data)}
-                              extraClassName={'buttonTable'}
-                            ></Button>
+                          <td>
+                            <div className='containerButtonTable'>
+                              <Button
+                                type={ButtonType.ERROR}
+                                value={"Delete"}
+                                onClick={() => deleteUser(data.id)}
+                                extraClassName={"buttonTable"}
+                              ></Button>
+                              <Button
+                                type={ButtonType.INFORMATION}
+                                value={"Update"}
+                                onClick={() => handleClickOnModal("edit", data)}
+                                extraClassName={"buttonTable"}
+                              ></Button>
+                            </div>
                           </td>
                         </>
                       </tr>
@@ -336,11 +342,11 @@ const Users = ({
                   ) : (
                     <tr>
                       <>
-                        <td>{'-'}</td>
-                        <td>{'-'}</td>
-                        <td>{'-'}</td>
-                        <td>{'-'}</td>
-                        <td>{'-'}</td>
+                        <td>{"-"}</td>
+                        <td>{"-"}</td>
+                        <td>{"-"}</td>
+                        <td>{"-"}</td>
+                        <td>{"-"}</td>
                       </>
                     </tr>
                   )}
@@ -349,10 +355,11 @@ const Users = ({
             </>
           )}
           <Pagination
-          limit={limit}
-          offsetState={offsetState}
-          totalItems={totalItems}
-          setOffsetState={setOffsetState}/>
+            limit={limit}
+            offsetState={offsetState}
+            totalItems={totalItems}
+            setOffsetState={setOffsetState}
+          />
           {usersStatus === ServerStatus.FETCHING && <Loader></Loader>}
         </section>
       </Layout>
