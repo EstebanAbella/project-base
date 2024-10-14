174b3258a826ec765e549563d63f3766369ec2eb
esteban <esteban.abella@hotmail.com>
2024-10-14 18:58:21 -0300

    update clientSelected and userSelected

--- pages/clientSelected/[param].tsx
+++ pages/clientSelected/[param].tsx
@@ -1,13 +1,14 @@
-import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
-import { RootState } from '../../redux/rootReducer'
-import { getClient } from '../../redux/client/actions'
-import { connect } from 'react-redux'
-import { ServerStatus } from '../../Utils/Types/global'
-import Loader from '../../components/Loader'
-import AccessConsume from '../../wrappers/auth/AccessConsume'
-import Layout from '../../components/Layout'
-import { ClientsReducerPropsType } from '../../Utils/Types/clientType'
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
+import { RootState } from "../../redux/rootReducer"
+import { getClient } from "../../redux/client/actions"
+import { connect } from "react-redux"
+import { ServerStatus } from "../../Utils/Types/global"
+import Loader from "../../components/Loader"
+import AccessConsume from "../../wrappers/auth/AccessConsume"
+import Layout from "../../components/Layout"
+import { ClientsReducerPropsType } from "../../Utils/Types/clientType"
+import Navigation from "../../components/Navigation"
 
 const mapStateToProps = (state: RootState) => {
   const clientsReducer = state.client
@@ -25,7 +26,11 @@ export type ClientPropType = {
   getClient: Function
 } & ClientsReducerPropsType
 
-const ClientSelected = ({ getClient, client, clientStatus }: ClientPropType) => {
+const ClientSelected = ({
+  getClient,
+  client,
+  clientStatus,
+}: ClientPropType) => {
   const router = useRouter()
   const { param } = router.query
 
@@ -37,26 +42,31 @@ const ClientSelected = ({ getClient, client, clientStatus }: ClientPropType) => 
 
   return (
     <AccessConsume>
-      <Layout isNavigation={true} newRoute={'/clients'} title={'All clients'}>
-        <>
-          {clientStatus === ServerStatus.FETCH && client && (
-            <section className="clientSelected">
-              <p>
-                Id: <span className="clientSpan">{client.id}</span>
-              </p>
-              <p>
-                Nombre: <span className="clientSpan">{client.name}</span>
-              </p>
-              <p>
-                E-mail: <span className="clientSpan">{client.email}</span>
-              </p>
-              <p>
-                Addres: <span className="clientSpan">{client.address}</span>
-              </p>
-            </section>
-          )}
-          {clientStatus === ServerStatus.FETCHING && <Loader></Loader>}
-        </>
+      <Layout>
+        <section className='clientSelected'>
+          <Navigation newRoute={"/clients"} title={"All clients"}></Navigation>
+          <section className='clientSelectedContainer'>
+            <>
+              {clientStatus === ServerStatus.FETCH && client && (
+                <div className='clientSelectedContainerData'>
+                  <p>
+                    Id: <span className='clientSpan'>{client.id}</span>
+                  </p>
+                  <p>
+                    Nombre: <span className='clientSpan'>{client.name}</span>
+                  </p>
+                  <p>
+                    E-mail: <span className='clientSpan'>{client.email}</span>
+                  </p>
+                  <p>
+                    Addres: <span className='clientSpan'>{client.address}</span>
+                  </p>
+                </div>
+              )}
+              {clientStatus === ServerStatus.FETCHING && <Loader></Loader>}
+            </>
+          </section>
+        </section>
       </Layout>
     </AccessConsume>
   )
--- pages/userSelected/[param].tsx
+++ pages/userSelected/[param].tsx
@@ -1,13 +1,14 @@
-import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
-import { RootState } from '../../redux/rootReducer'
-import { getUser } from '../../redux/user/actions'
-import { UsersReducerPropsTypes } from '../../Utils/Types/userType'
-import { connect } from 'react-redux'
-import { ServerStatus } from '../../Utils/Types/global'
-import Loader from '../../components/Loader'
-import AccessConsume from '../../wrappers/auth/AccessConsume'
-import Layout from '../../components/Layout'
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
+import { RootState } from "../../redux/rootReducer"
+import { getUser } from "../../redux/user/actions"
+import { UsersReducerPropsTypes } from "../../Utils/Types/userType"
+import { connect } from "react-redux"
+import { ServerStatus } from "../../Utils/Types/global"
+import Loader from "../../components/Loader"
+import AccessConsume from "../../wrappers/auth/AccessConsume"
+import Layout from "../../components/Layout"
+import Navigation from "../../components/Navigation"
 
 const mapStateToProps = (state: RootState) => {
   const usersReducer = state.user
@@ -36,26 +37,31 @@ const UserSelected = ({ getUser, user, userStatus }: UserPropType) => {
   }, [param])
   return (
     <AccessConsume>
-      <Layout isNavigation={true} newRoute={'/users'} title={'All users'}>
-        <>
-          {userStatus === ServerStatus.FETCH && user && (
-            <section className="userSelected">
-              <p>
-                Id: <span className="userSpan">{user.id}</span>
-              </p>
-              <p>
-                Nombre: <span className="userSpan">{user.name}</span>
-              </p>
-              <p>
-                E-mail: <span className="userSpan">{user.email}</span>
-              </p>
-              <p>
-                Role: <span className="userSpan">{user.role}</span>
-              </p>
-            </section>
-          )}
-          {userStatus === ServerStatus.FETCHING && <Loader></Loader>}
-        </>
+      <Layout>
+        <section className='userSelected'>
+          <Navigation newRoute={"/users"} title={"All users"}></Navigation>
+          <section className='userSelectedContainer'>
+            <>
+              {userStatus === ServerStatus.FETCH && user && (
+                <div className='userSelectedContainerData'>
+                  <p>
+                    Id: <span className='userSpan'>{user.id}</span>
+                  </p>
+                  <p>
+                    Nombre: <span className='userSpan'>{user.name}</span>
+                  </p>
+                  <p>
+                    E-mail: <span className='userSpan'>{user.email}</span>
+                  </p>
+                  <p>
+                    Role: <span className='userSpan'>{user.role}</span>
+                  </p>
+                </div>
+              )}
+              {userStatus === ServerStatus.FETCHING && <Loader></Loader>}
+            </>
+          </section>
+        </section>
       </Layout>
     </AccessConsume>
   )
